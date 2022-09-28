4c2ca347930bbac6e42e125aa42cec5b0e45530d
coreyhsGames <83317368+coreyhsGames@users.noreply.github.com>
2022-09-28 20:33:10 +1300

    Added Colour Themes.

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const root = document.querySelector(":root"),
+    inputs = document.querySelectorAll("input[name='theme']");
+
+const theme = localStorage.getItem("theme-color");
+
+const updateRoot = value => root.style.setProperty("--theme-color", `var(--${value})`);
+
+for (const input of inputs) {
+    if (theme && input.value === theme) {
+        input.checked = true;
+
+        updateRoot(theme);
+    }
+
+    input.onchange = e => {
+        updateRoot(e.target.value);
+
+        localStorage.setItem("theme-color", e.target.value);
+    }
+}
